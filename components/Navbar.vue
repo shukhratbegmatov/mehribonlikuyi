337d05b0508cb5bfa387fc22afa76fe2f54f72a8
Пользователь <shuukhratbegmatov@gmail.com>
2022-04-17 13:29:10 +0500

    begin front mehribonlik uyi

---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container">
+    <div class="main_nav">
+      <div class="navbar_brand">
+        <NuxtLink to="/">Mehribonlik uy</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Mehribonlik uylari
+    <Navbar></Navbar>
   </div>
 </template>
 
